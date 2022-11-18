10e73a5fdd7bbc555acd9a280a0cfdba6f1c9da5
최지연 <choi02647@naver.com>
2022-11-18 12:14:47 +0900

    added BeerListViewController

---
+++ Brewery/BeerListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  BeerListViewController.swift
+//  Brewery
+//
+//  Created by Jiyeon Choi on 2022. 11. 18..
+//
+
+import UIKit
+
+class BeerListViewController: UITableViewController {
+    
+}
