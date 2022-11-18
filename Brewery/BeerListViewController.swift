84fcb42089593a62e22e79c20a1826f2bf069f78
최지연 <choi02647@naver.com>
2022-11-18 14:33:56 +0900

    added beer data

--- Brewery/BeerListViewController.swift
+++ Brewery/BeerListViewController.swift
@@ -8,5 +8,37 @@
 import UIKit
 
 class BeerListViewController: UITableViewController {
+    var beerList = [Beer]()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //UINavigationController 설정
+        title = "브루어리"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //UITableView 설정
+        tableView.prefetchDataSource = self
+        tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
+        tableView.rowHeight = 150        
+    }
+}
+
+//UITableView DataSource, Delegate
+extension BeerListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beerList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
+        
+        let beer = beerList[indexPath.row]
+        cell.configure(with: beer)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     
+    }
 }
