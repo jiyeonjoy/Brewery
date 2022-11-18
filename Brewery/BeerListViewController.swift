2c523d8d32ca40136aaa31d174f50c326984e3a9
최지연 <choi02647@naver.com>
2022-11-18 14:47:24 +0900

    api 연동 done

--- Brewery/BeerListViewController.swift
+++ Brewery/BeerListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class BeerListViewController: UITableViewController {
     var beerList = [Beer]()
+    var dataTasks = [URLSessionTask]()
+    var currentPage = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,9 @@ class BeerListViewController: UITableViewController {
         //UITableView 설정
         tableView.prefetchDataSource = self
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
-        tableView.rowHeight = 150        
+        tableView.rowHeight = 150
+        
+        fetchBeer(of: currentPage)
     }
 }
 
@@ -43,5 +47,71 @@ extension BeerListViewController {
         let detailViewController = BeerDetailViewController()
         detailViewController.beer = selectedBeer
         self.show(detailViewController, sender: nil)
+    }
+}
+
+extension BeerListViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard currentPage != 1 else { return }
+        
+        indexPaths.forEach {
+            if ($0.row + 1)/25 + 1 == currentPage {
+                self.fetchBeer(of: currentPage)
+            }
+        }
+    }
+}
+
+private extension BeerListViewController {
+    func fetchBeer(of page: Int) {
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)"),
+            dataTasks.firstIndex(where: { $0.originalRequest?.url == url }) == nil else { return } //이미 Beer를 패칭했음. 그렇지 않다면,
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) {[weak self] data, response, error in
+            guard error == nil,
+                  let self = self,
+                  let response = response as? HTTPURLResponse,
+                  let data = data,
+                  let beers = try? JSONDecoder().decode([Beer].self, from: data) else {
+                    print("ERROR: URLSession data task error \(error?.localizedDescription ?? "")")
+                    return
+            }
+            
+            switch response.statusCode {
+            case (200...299):
+                self.beerList += beers
+                self.currentPage += 1
+
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case (400...499):
+                print(
+                    """
+                    ERROR: Client ERROR \(response.statusCode)
+                    Response: \(response)
+                    """
+                )
+            case (500...599):
+                print(
+                    """
+                    ERROR: Server ERROR \(response.statusCode)
+                    Response: \(response)
+                    """
+                )
+            default:
+                print(
+                    """
+                    ERROR: \(response.statusCode)
+                    Response: \(response)
+                    """
+                )
+            }
+        }
+        dataTask.resume()
+        dataTasks.append(dataTask)
     }
 }
